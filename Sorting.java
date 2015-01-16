ed23f01a4b671e0947703ed7c516e18263495699
emmaavera <emma.avera.stuy@gmail.com>
2015-01-16 15:11:42 +0000

    Update and rename Planner.java to Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,9 @@
+public class Planner {
+  
+  protected ArrayList<Arraylist> hwTime = new ArrayList<ArrayList>() //Used long as the default for time, but change as needed
+  
+  /*Given an arraylist of times, either as entered by the user to estimate, or by data gathered from a "Stop" button
+  this method finds the standard deviation, factoring out outliers to give users. This estimated time will then be used to 
+  give the user a suggested ammount of time to input.*/
+  
+}
