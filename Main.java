a5f80f67eca14a1419ad49f072d71386efbc1fa6
Samuel Zhang <samuelzhang3@gmail.com>
2015-01-21 20:51:20 -0500

    Second Window GUI (Add Task)

--- Main.java
+++ Main.java
@@ -5,27 +5,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main extends JFrame implements ActionListener{
-
-  public void actionPerformed(ActionEvent event) {
-    JFrame taskFrame = new JFrame();
-    taskFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    taskFrame.setSize(800, 600);
-    taskFrame.setVisible(true);
-
-    //Task Details
-    JTextField name = new JTextField("Name: ");
-    JRadioButton one = new JRadioButton("1");    
-    JRadioButton two = new JRadioButton("2");    
-    JRadioButton three = new JRadioButton("3");    
-    JRadioButton four = new JRadioButton("4");    
-    JRadioButton five = new JRadioButton("5");    
-  }
-
-  public static void main(String[] args) {
-    Main buttonOperation = new Main();
-
+  public Main() {
     //Main Frame
     JFrame frame = new JFrame();
+    frame.setTitle("Time Planner");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(800, 600);
     frame.setVisible(true);
@@ -33,12 +16,40 @@ public class Main extends JFrame implements ActionListener{
     //New Task Button
     JButton addTask = new JButton("New Task");
     frame.getContentPane().add(BorderLayout.SOUTH, addTask);    
-    addTask.addActionListener(buttonOperation);
+    addTask.addActionListener(this); //Listen to click
 
     //Start Timer
     JButton startTimer = new JButton("Start Current Task");
     frame.getContentPane().add(BorderLayout.EAST, startTimer);
+  }
 
 
+  public void actionPerformed(ActionEvent event) {
+    //Frame
+    JFrame taskFrame = new JFrame();
+    taskFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    taskFrame.setSize(300, 300);
+    taskFrame.setVisible(true);
+
+    //Panel
+    JPanel panel = new JPanel();
+    panel.setBackground(Color.darkGray);
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));  //Vertical Panel
+    
+    //Task Details
+    JTextField name = new JTextField("Name: ");
+    JComboBox<String> priority = new JComboBox<String>();
+    priority.addItem("Important");
+    priority.addItem("Not Important");
+
+    //Add Text Field/Radial Buttons
+    panel.add(name);
+    panel.add(priority);
+
+    taskFrame.getContentPane().add(panel);
+  }
+
+  public static void main(String[] args) {
+    Main GUI = new Main();
   }
 }
