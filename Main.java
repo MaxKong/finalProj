30d3988c578c07484a504410f02d6a003a588c3b
Samuel Zhang <samuelzhang3@gmail.com>
2015-01-21 21:49:04 -0500

    Indiv. New Task Formatting

--- Main.java
+++ Main.java
@@ -29,31 +29,44 @@ public class Main extends JFrame implements ActionListener{
   }
 
 
-  public void actionPerformed(ActionEvent event) {
+  public void actionPerformed(ActionEvent event) { //New Task Button
     //Frame
     JFrame taskFrame = new JFrame();
+    taskFrame.setSize(800, 600);
     taskFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    taskFrame.setSize(300, 300);
     taskFrame.setVisible(true);
 
     //Panel
     JPanel taskPanel = new JPanel();
-    taskPanel.setBackground(Color.darkGray);
+    taskPanel.setBackground(Color.gray);
     taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));  //Vertical Panel
     
     //Task Details
-    JTextField name = new JTextField("Name: "); 
+    JLabel name = new JLabel("Name: ");
+    JTextField nameText = new JTextField(20); 
+    nameText.setMaximumSize(new Dimension(Integer.MAX_VALUE, nameText.getMinimumSize().height)); //1-line box
+
+    JLabel importance = new JLabel("Priority (1: Lowest, 5: highest): ");
     JComboBox<String> priority = new JComboBox<String>(); //Drop Down Menu
-    priority.addItem("Important");
-    priority.addItem("Not Important");
+    priority.setMaximumSize(new Dimension(Integer.MAX_VALUE, priority.getMinimumSize().height)); //small menu
+    priority.addItem("1");
+    priority.addItem("2");
+    priority.addItem("3");
+    priority.addItem("4");
+    priority.addItem("5");
+
     JButton submit = new JButton("Submit"); //Submit button, close window, MUST ADD SAVE INFO
 
     //Add Text Field/Radial Buttons
     taskPanel.add(name);
+    taskPanel.add(nameText);
+    taskPanel.add(importance);
     taskPanel.add(priority);
     taskPanel.add(submit);
 
+    //Add Panel and Fit Window to Content
     taskFrame.getContentPane().add(taskPanel);
+
   }
 
   public static void main(String[] args) {
