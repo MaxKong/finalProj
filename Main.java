83b165ea1e46f490f5453c192c48a3f8642c76de
Samuel Zhang <samuelzhang3@gmail.com>
2015-01-22 23:30:31 -0500

    Inner Classes - Submit Button Activitygit add Main.java :D

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main extends JFrame implements ActionListener{
 
     //New Task Button
     JButton addTask = new JButton("New Task");
-    addTask.addActionListener(this); //Listen to click
+    addTask.addActionListener(new NewTask()); //Listen to click
 
     //Start Timer
     JButton startTimer = new JButton("Start Current Task");
@@ -27,47 +27,72 @@ public class Main extends JFrame implements ActionListener{
     panel.add(startTimer);
     frame.getContentPane().add(BorderLayout.EAST, panel);
   }
-
-  public void actionPerformed(ActionEvent event) { //New Task Button
-    //Frame
-    JFrame taskFrame = new JFrame();
-    taskFrame.setSize(800, 600);
-    taskFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    taskFrame.setVisible(true);
-
-    //Panel
-    JPanel taskPanel = new JPanel();
-    taskPanel.setBackground(Color.gray);
-    taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));  //Vertical Panel
+  
+  public void actionPerformed(ActionEvent event) { //Possibly for Displaying Added Tasks (Max's Text File)
     
-    //Task Details
-    JLabel name = new JLabel("Name: ");
-    JTextField nameText = new JTextField(20); 
-    nameText.setMaximumSize(new Dimension(Integer.MAX_VALUE, nameText.getMinimumSize().height)); //1-line box
-
-    JLabel importance = new JLabel("Priority (1: Lowest, 5: highest): ");
-    JComboBox<String> priority = new JComboBox<String>(); //Drop Down Menu
-    priority.setMaximumSize(new Dimension(Integer.MAX_VALUE, priority.getMinimumSize().height)); //small menu
-    priority.addItem("1");
-    priority.addItem("2");
-    priority.addItem("3");
-    priority.addItem("4");
-    priority.addItem("5");
-
-    JButton submit = new JButton("Submit"); //Submit button, close window, MUST ADD SAVE INFO
-
-    //Add Text Field/Radial Buttons
-    taskPanel.add(name);
-    taskPanel.add(nameText);
-    taskPanel.add(importance);
-    taskPanel.add(priority);
-    taskPanel.add(submit);
-
-    //Add Panel and Fit Window to Content
-    taskFrame.getContentPane().add(taskPanel);
   }
 
+  class NewTask implements ActionListener {
+    JFrame taskFrame; //So Submit button can detect and close it
+
+    public void actionPerformed(ActionEvent event) { //New Task Button
+      //Frame
+      taskFrame = new JFrame();
+      taskFrame.setSize(800, 600);
+      taskFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+      taskFrame.setVisible(true);
+      
+      //Panel
+      JPanel taskPanel = new JPanel();
+      taskPanel.setBackground(Color.gray);
+      taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));  //Vertical Panel
+      
+      //Task Details ------------------------------------------
+
+      //TextBox
+      JLabel taskName = new JLabel("Name: ");
+      JTextField nameText = new JTextField(20); 
+      //One line Size
+      nameText.setMaximumSize(new Dimension(Integer.MAX_VALUE, nameText.getMinimumSize().height));
+      
+      //Drop Down Menu
+      JLabel importance = new JLabel("Priority (1: Lowest, 5: highest): ");
+      JComboBox<String> priority = new JComboBox<String>(); //Drop Down Menu
+      //One line Size
+      priority.setMaximumSize(new Dimension(Integer.MAX_VALUE, priority.getMinimumSize().height));
+      priority.addItem("1");
+      priority.addItem("2");
+      priority.addItem("3");
+      priority.addItem("4");
+      priority.addItem("5");
+      
+      JButton submit = new JButton("Submit"); //Submit button, close window, MUST ADD SAVE INFO
+      submit.addActionListener(new SubmitListener());
+
+      //Add to Panel
+      taskPanel.add(taskName);
+      taskPanel.add(nameText);
+      taskPanel.add(importance);
+      taskPanel.add(priority);
+      taskPanel.add(submit);
+      
+      //Add Panel and Fit Window to Content
+      taskFrame.getContentPane().add(taskPanel);
+    
+    }
+
+      class SubmitListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+          taskFrame.dispose();
+        }
+      }
+
+  }
+
+
   public static void main(String[] args) {
-    Main GUI = new Main();
+    Main GUI = new Main(); //ActionEvent must access an object
+
+ 
   }
 }
