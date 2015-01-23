ddd587c3073f59759c743fb4bef5e0a234ce9a95
Samuel Zhang <samuelzhang3@gmail.com>
2015-01-23 01:14:58 -0500

    Action not fixed. On StackExchange. Timer not refreshing

--- Timer.java
+++ Timer.java
@@ -1,72 +1,101 @@
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Timer extends JFrame {
+public class Timer extends JFrame implements ActionListener {
     
-    protected int minutes;
-    protected int seconds;
-    protected int totalTime;
-    protected int elapsedTime; 
+  protected int minutes;
+  protected int seconds;
+  protected int totalTime;
+  protected int elapsedTime; 
 
-    public Timer() {
-	minutes = 0;
-	seconds = 0;
-	totalTime = 0;
+  public Timer() {
+    minutes = 0;
+    seconds = 0;
+    totalTime = 0;
+  }
+
+  public Timer(int min, int sec) {
+    minutes = min;
+    seconds = sec;
+    totalTime = minutes*60 + seconds; //in seconds
+  }
+
+  public String secondsToMinutes(int sec) {
+    String s = "";
+    int minutes =  sec/60;
+    int seconds = sec%60;
+    if (minutes < 10 && seconds < 10) {
+      s = "0" + minutes + ":0" + seconds; 
     }
-
-    public Timer(int min, int sec) {
-	minutes = min;
-	seconds = sec;
-	totalTime = minutes*60 + seconds; //in seconds
-    }
-
-    public String secondsToMinutes(int sec) {
-	String s = "";
-	int minutes =  sec/60;
-	int seconds = sec%60;
-	if (seconds < 10){ 
+    else if (seconds < 10){ 
 	    //System.out.println ( minutes + ":0" + seconds);
 	    s = minutes + ":0" + seconds;
-	}
-	else{
+    }
+    else if (minutes < 10) {
+      s = "0" + minutes + ":" + seconds; 
+    }
+    else{
 	    //System.out.println ( minutes + ":" + seconds);
 	    s = minutes + ":" + seconds;
-	}
-	return s;
     }
+    return s;
+  }
 
-    public String countDown() {
-	String s = "";
-	long start = System.currentTimeMillis();
-	boolean isSecond = false;
-	secondsToMinutes(totalTime - elapsedTime);
-	while (!isSecond) {//basically a delay factor
+  public String countDown() {
+    String s = "";
+    long start = System.currentTimeMillis();
+    boolean isSecond = false;
+    secondsToMinutes(totalTime - elapsedTime);
+    while (!isSecond) {//basically a delay factor
 	    if (System.currentTimeMillis() - start >= 1000) {
-		//start = System.currentTimeMillis();
-		elapsedTime++;
-		isSecond = true;
+        elapsedTime++;
+        isSecond = true;
 	    }
-	}
-	return secondsToMinutes(totalTime - elapsedTime);
     }
-    
-    public static void main(String args[]) {
-	Timer t = new Timer(1, 10);//feed min & sec ofc	
-	JFrame j = new JFrame();
-	JPanel panel = new JPanel();
-	panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-	//JButton b = new JButton("Start Timer");
-	j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	j.setSize(500, 500);
-	j.setVisible(true);
-	
-	JButton l = new JButton("Hih");
-	panel.add(l);
-	j.getContentPane().add(panel);	
-	while (t.elapsedTime < t.totalTime) {	
-	    System.out.println("fdfr");
-	   
-	}
+    return secondsToMinutes(totalTime - elapsedTime);
+  }
+
+  public void refresh(JLabel l, JPanel p) {
+    while (elapsedTime < totalTime) {
+      System.out.println(countDown());
+      l.setText(countDown());
+      p.validate();
+      p.repaint();
     }
+  }
+
+  public void actionPerformed(ActionEvent event) {
+
+    Font bigFont = new Font ("Heletica", Font.BOLD, 170);
+    Font littleFont = new Font ("Heletica", Font.BOLD, 60);
+
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    panel.setBackground(Color.gray);
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); //Vertical Box Layout
+
+    JLabel label = new JLabel( secondsToMinutes(totalTime) );
+    JButton button = new JButton( "PAUSE" );
+
+    panel.add(label);
+    panel.add(button);
+    frame.getContentPane().add(BorderLayout.CENTER, panel);
+
+    button.setFont(littleFont);
+    label.setFont(bigFont);
+
+    frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+    frame.setSize(800,600);
+    frame.setVisible(true);
+
+    /*    while (elapsedTime < totalTime) {
+      System.out.println(countDown());
+      label.setText(countDown());
+
+      }*/
+    refresh(label, panel);
+  }
 }
